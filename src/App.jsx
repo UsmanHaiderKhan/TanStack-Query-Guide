185b2b451b28c4f3b13c4b24fe06fa8ecd9d1091
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-10 11:43:36 +0200

    ♻️ Update : Add loader and action to EditEvent route
    The EditEvent route now includes loader and action functions for improved data handling and form submission. Also removed redundant QueryClient import.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import {createBrowserRouter, Navigate, RouterProvider,} from 'react-router-dom';
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import EditEvent, {loader as editEventLoader, action as editEventAction} from './components/Events/EditEvent.jsx';
+import { QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "./Utils/http.js";
 
 const router = createBrowserRouter([
@@ -30,6 +30,8 @@ const router = createBrowserRouter([
             {
                 path: '/events/:id/edit',
                 element: <EditEvent/>,
+                loader: editEventLoader,
+                action: editEventAction,
             },
         ],
     },

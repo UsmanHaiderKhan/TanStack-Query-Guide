0888a356d4dceb50b78a216811d9850c73489ea3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-11 11:44:14 +0200

    ♻️ Update : Add updateEvent API and enhance fetchEvents params
    Introduces an updateEvent function for updating events via PUT requests. The fetchEvents function now supports an optional 'max' parameter for limiting results and improves query string construction. Also refactors code for consistent formatting and destructuring.

--- src/Utils/http.js
+++ src/Utils/http.js
@@ -1,15 +1,21 @@
-import { QueryClient } from '@tanstack/react-query';
+import {QueryClient} from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
 
-export async function fetchEvents({ signal, searchTerm }) {
+export async function fetchEvents({signal, searchTerm, max}) {
+    console.log(searchTerm);
     let url = 'http://localhost:3000/events';
 
-    if (searchTerm) {
+    if (searchTerm && max) {
+        url += '?search=' + searchTerm + '&max=' + max;
+
+    } else if (searchTerm) {
         url += '?search=' + searchTerm;
+    } else if (max) {
+        url += '?max=' + max;
     }
 
-    const response = await fetch(url, { signal: signal });
+    const response = await fetch(url, {signal: signal});
 
     if (!response.ok) {
         const error = new Error('An error occurred while fetching the events');
@@ -18,10 +24,11 @@ export async function fetchEvents({ signal, searchTerm }) {
         throw error;
     }
 
-    const { events } = await response.json();
+    const {events} = await response.json();
 
     return events;
 }
+
 
 export async function createNewEvent(eventData) {
     const response = await fetch(`http://localhost:3000/events`, {
@@ -39,13 +46,13 @@ export async function createNewEvent(eventData) {
         throw error;
     }
 
-    const { event } = await response.json();
+    const {event} = await response.json();
 
     return event;
 }
 
-export async function fetchSelectableImages({ signal }) {
-    const response = await fetch(`http://localhost:3000/events/images`, { signal });
+export async function fetchSelectableImages({signal}) {
+    const response = await fetch(`http://localhost:3000/events/images`, {signal});
 
     if (!response.ok) {
         const error = new Error('An error occurred while fetching the images');
@@ -54,13 +61,13 @@ export async function fetchSelectableImages({ signal }) {
         throw error;
     }
 
-    const { images } = await response.json();
+    const {images} = await response.json();
 
     return images;
 }
 
-export async function fetchEvent({ id, signal }) {
-    const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+export async function fetchEvent({id, signal}) {
+    const response = await fetch(`http://localhost:3000/events/${id}`, {signal});
 
     if (!response.ok) {
         const error = new Error('An error occurred while fetching the event');
@@ -69,18 +76,38 @@ export async function fetchEvent({ id, signal }) {
         throw error;
     }
 
-    const { event } = await response.json();
+    const {event} = await response.json();
 
     return event;
 }
 
-export async function deleteEvent({ id }) {
+
+export async function deleteEvent({id}) {
     const response = await fetch(`http://localhost:3000/events/${id}`, {
         method: 'DELETE',
     });
 
     if (!response.ok) {
         const error = new Error('An error occurred while deleting the event');
+        error.code = response.status;
+        error.info = await response.json();
+        throw error;
+    }
+
+    return response.json();
+}
+
+export async function updateEvent({id, event}) {
+    const response = await fetch(`http://localhost:3000/events/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({event}),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        const error = new Error('An error occurred while updating the event');
         error.code = response.status;
         error.info = await response.json();
         throw error;

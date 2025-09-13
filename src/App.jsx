8fa528edc2a9b532293f07f9d2171ba3564e13cd
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-13 22:34:34 +0200

    🛠️ Fix :  Refactor query client and add event/image API functions
    Moved QueryClient instantiation to http.js and exported it for use in App.jsx. Added createNewEvent and fetchSelectableImages functions to centralize API calls for events and images, improving code organization and reusability.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "./Utils/http.js";
 
 const router = createBrowserRouter([
     {
@@ -33,7 +34,7 @@ const router = createBrowserRouter([
         ],
     },
 ]);
-const queryClient = new QueryClient();
+
 
 function App() {
     return (

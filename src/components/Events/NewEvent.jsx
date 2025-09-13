c66da9de89f65e6d29c2a92c61cb73972261db23
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-13 22:35:06 +0200

    ♻️ Update : add event creation mutation to NewEvent component
    Integrates react-query's useMutation for creating new events, handles loading and error states, and invalidates the events query on success. UI feedback is provided for pending and error states.

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,43 @@
 import { Link, useNavigate } from 'react-router-dom';
+import {useMutation} from "@tanstack/react-query";
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import {createNewEvent, queryClient} from "../../Utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
-
-  function handleSubmit(formData) {}
+  const {mutate, isPending, isError, error}=useMutation({
+   // mutationKey: ['new-event'],
+      mutationFn: createNewEvent,
+      onSuccess: () =>{
+          queryClient.invalidateQueries({queryKey: ['events']});
+            navigate('/events');
+      }
+  })
+  function handleSubmit(formData) {
+      mutate({event: formData});
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+          {isPending && (<p>Sending data...</p>)}
+          {!isPending && (
+              <>
+                  <Link to="../" className="button-text">
+                      Cancel
+                  </Link>
+                  <button type="submit" className="button">
+                      Create
+                  </button>
+              </>
+          )}
+
       </EventForm>
+        {isError && <ErrorBlock title="Failed to create new Event." message={error.info?.message ||
+            'Failed to create event. please check your inputs and try again later...!'}/>}
     </Modal>
   );
 }

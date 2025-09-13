0f8ee4c3019f60496aedd7202fa850be44fb4035
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-09-14 00:38:02 +0200

    ♻️ Update : Add event details fetching and delete functionality
    EventDetails now fetches event data using React Query and displays dynamic details. Added delete event functionality with mutation and navigation on success. Error handling and loading states are also included.

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,66 @@
-import { Link, Outlet } from 'react-router-dom';
+import {Link, Outlet, useNavigate, useParams} from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import {useMutation, useQuery} from "@tanstack/react-query";
+import {deleteEvent, fetchEvent, queryClient} from "../../Utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
-    </>
-  );
+    const params = useParams();
+    const navigate = useNavigate();
+    const {data, isPending, isError, error} =
+        useQuery({
+            queryKey: ['event-details', {id: params.id}],
+            queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+        });
+
+// Delete, Edit
+  const {mutate}  = useMutation({
+        mutationKey: ['delete-event'],
+        mutationFn: deleteEvent,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['events'], refetchType: 'none'});
+            navigate('/events');
+        }
+    })
+   function handleDelete() {
+        mutate({id: params.id});
+   }
+    return (
+        <>
+            <Outlet/>
+            <Header>
+                <Link to="/events" className="nav-item">
+                    View all Events
+                </Link>
+            </Header>
+            <article id="event-details">
+                {isPending && <p>Loading event details...</p>}
+                {data && (
+                    <>
+                        <header>
+                            <h1>{data.title}</h1>
+                            <nav>
+                                <button onClick={handleDelete}>Delete</button>
+                                <Link to="edit">Edit</Link>
+                            </nav>
+                        </header>
+                        <div id="event-details-content">
+                            <img src={`http://localhost:3000/${data.image}`} alt={data.title}/>
+                            <div id="event-details-info">
+                                <div>
+                                    <p id="event-details-location">{data.location}</p>
+                                    <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} - {data.time}</time>
+                                </div>
+                                <p id="event-details-description">{data.description}</p>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+                {isError && <ErrorBlock title="Failed to load event details."
+                                        message={error.info?.message || 'Failed to load event details. please try again later...!'}/>}
+            </article>
+        </>
+    );
 }
